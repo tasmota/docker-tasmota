a74984b93d5477f1f9a3fcb4e82c2da3b29dff5d
blakadder <arguson@gmail.com>
2019-11-16 22:55:27 +0100

    all working

--- compile.sh
+++ compile.sh
@@ -1,57 +1,86 @@
 #!/bin/bash
+
+# Copy this bash script to a directory below /Tasmota and run from there
+
 CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
 rundir=$(dirname $(readlink -f $0))
 
-## Display chosen builds
-echo -e "Running Docker Tasmota for builds:"
+## Check whether Tasmota/ exists and fetch newest Tasmota version from development branch
+if test -d `pwd`"/Tasmota"; then
+    echo -e "Checking Tasmota GitHub for the most recent development version"
+    cd Tasmota
+    git fetch --all
+    git reset --hard origin/development
+    git pull
+    cd $rundir
+    # Check if docker installed
+    if [[ "$(type -t docker)" == "file" ]] ; then
+        ## Display chosen builds
+        echo -e "Running Docker Tasmota for builds:"
+        ## Display builds
+        if  [ $# -eq 0 ]; then
+            echo -e "defined in platformio.ini\n\nNOTICE: \e[31mDefault is ALL BUILDS!!!!\e[0m\n"
+            sleep 3
+            else
+                for build in "$@"
+                do
+                    echo -e "$build"
+                    sleep 1
+                done
+        fi
 
-## Display builds
-if  [ $# -eq 0 ]; then
-    echo -e "defined in platformio.ini\n\nNOTICE: \e[31mDefault is ALL BUILDS!!!!\e[0m\n"
-    sleep 3
-else
-    for build in "$@"
-    do
-        echo -e "$build"
-        sleep 1
-    done
-fi
-
-## fetch newest Tasmota version from development
-echo -e "Checking Tasmota GitHub for the most recent development version"
-cd `pwd`/Tasmota
-git fetch --all 
-git reset --hard origin/development 
-git pull
-cd $rundir
-
-## Check script dir for custom files (platformio.ini or user_config_override.h)
-if test -f "platformio.ini"; then
-    cp "$rundir"/platformio.ini "$rundir"/Tasmota/platformio.ini
-    echo -e "Using your platformio.ini and overwriting the default one"
-fi
-if test -f "user_config_override.h"; then
-    cp "$rundir"/user_config_override.h "$rundir"/Tasmota/tasmota/user_config_override.h
-    echo -e "Using your user_config_override.h and overwriting the default one\n"
-fi
-## Run container with provided arguments
-echo -n "Compiling..."
-    if  [ $# -ne 0 ]; then
-    docker run -it --rm -v `pwd`/Tasmota:/tasmota blakadder/docker-tasmota $(printf ' -e %s' $@) > docker-tasmota.log 2>&1 
-else
-    docker run -it --rm -v `pwd`/Tasmota:/tasmota blakadder/docker-tasmota > docker-tasmota.log 2>&1 
-    echo "Find your builds in .pioenvs/<build-flavour>/firmware.bin"
-
-fi
-echo -e "\\r${CHECK_MARK} Finished!  \tCompilation log in docker-tasmota.log\n"
-## After docker is completed copy firmware to script dir and rename to buildname
-for build in "$@"
-do
-  cp "$rundir"/Tasmota/.pioenvs/"$build"/firmware.bin "$rundir"/"$build".bin
-    if test -f "$build".bin; then
-        echo -e "Completed! Your firmware is $rundir/$build.bin\n"
+        ## Check script dir for custom platformio.ini or user_config_override.h
+        if test -f "platformio.ini"; then
+            cp platformio.ini Tasmota/platformio.ini
+            echo -e "Using your platformio.ini and overwriting the default file"
+        fi
+        if test -f "user_config_override.h"; then
+            cp user_config_override.h Tasmota/tasmota/user_config_override.h
+            echo -e "Using your user_config_override.h and overwriting the default file\n"
+        fi
+        ## Run container with provided arguments
+        echo -n "Compiling..."
+        if  [ $# -ne 0 ]; then
+            docker run -it --rm -v `pwd`/Tasmota:/tasmota blakadder/docker-tasmota $(printf ' -e %s' $@) > docker-tasmota.log 2>&1 
+            else
+            docker run -it --rm -v `pwd`/Tasmota:/tasmota blakadder/docker-tasmota > docker-tasmota.log 2>&1 
+            echo "Find your builds in .pioenvs/<build-flavour>/firmware.bin"
+        fi
+        echo -e "\\r${CHECK_MARK} Finished!  \tCompilation log in docker-tasmota.log\n"
+        ## After docker is completed copy firmware to script dir and rename to buildname
+        for build in "$@"
+        do
+        cp "$rundir"/Tasmota/.pioenvs/"$build"/firmware.bin "$rundir"/"$build".bin
+            if test -f "$build".bin; then
+                echo -e "Completed! Your firmware is $rundir/$build.bin\n"
+            else
+                echo -e "\e[31m\e[5mWARNING:\e[0m"
+                echo -e "Something went wrong while compiling $build. Check compilation log\n"
+            fi  
+        done
     else
-        echo -e "\e[31m\e[5mWARNING:\e[0m"
-        echo -e "Something went wrong while compiling $build. Check compilation log\n"
-    fi  
-done
+        if [[ "$(type -t curl)" != "file" ]] ; then
+            echo -e "Please install "curl" to proceed"
+            exit 1
+        else
+        echo -e "\nNo Docker detected. Please install docker:\n\n\tcurl -fsSL https://get.docker.com -o get-docker.sh\n\tsh get-docker.sh\n"
+        fi
+    fi
+else
+    if [[ "$(type -t git)" == "file" ]] ; then
+        echo -e "\nNo Tasmota Git repository found in directory.\nDo you wish to clone Tasmota GitHub repository to current directory?"
+        read -p "Enter to exit, "yes" to proceed: " answer
+            case ${answer:0:1} in
+                y|yes )
+                    git clone https://github.com/arendst/Tasmota.git
+                    ./$(basename $0) && exit   
+                ;;
+                * )
+                    exit 1
+                ;;
+            esac
+    else
+        echo -e "\nPlease install "git" to proceed:\n\n\tDebian/Ubuntu/Mint:\tsudo apt-get install git\n\tFedora:\t\t\tsu -c 'yum install git'\n\topenSUSE:\t\tsudo zypper in git\n"
+        exit 1
+    fi
+fi
