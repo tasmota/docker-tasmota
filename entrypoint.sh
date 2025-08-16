9b1719b718c88990ae9f174c41b3362e6d592228
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-08-16 15:21:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 # configure build via environment
 #!/bin/bash
 
+export UV_PYTHON="/.platformio/penv/bin/python"
+export UV_CACHE_DIR="/.cache/uv"
+export PATH="/.platformio/penv/bin:$PATH"
+
 TASMOTA_VOLUME='/tasmota'
 
+chmod -R 777 /.platformio /.cache /.local
 
 if [ -d $TASMOTA_VOLUME ]; then
 	cd $TASMOTA_VOLUME
