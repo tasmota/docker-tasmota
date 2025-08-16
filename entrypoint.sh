3c42e15e97a1156690b9e3ce7931a57a8a66bf86
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-08-16 16:31:54 +0200

    remove chmld 777 use unmask 000 instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 # configure build via environment
 #!/bin/bash
 
-# Configure build environment to use penv virtual environment
+# Set umask so new files/directories get full permissions automatically
+umask 000
+
+# Configure build environment to use penv virtual environment  
 export UV_PYTHON="/.platformio/penv/bin/python"
 export UV_CACHE_DIR="/.cache/uv"
 export PATH="/.platformio/penv/bin:$PATH"
@@ -10,13 +13,11 @@ export PYTHONPATH="/.platformio/penv/lib/python3.13/site-packages:$PYTHONPATH"
 
 TASMOTA_VOLUME='/tasmota'
 
-chmod -R 777 /.platformio /.cache /.local
-
 if [ -d $TASMOTA_VOLUME ]; then
     cd $TASMOTA_VOLUME
     echo "Compiling Tasmota..."
-    echo "Using Python: $(which python)"
-    echo "Using PlatformIO: $(which pio)"
+#    echo "Using Python: $(which python)"
+#    echo "Using PlatformIO: $(which pio)"
     pio run $@
     echo "All done! Find your builds in Tasmota/build_output/firmware/"
 else
