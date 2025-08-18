c31bb7ca4cf4e35d339130cfa4b8cb6eb18760b4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-08-18 11:27:06 +0200

    unmask 000

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 # configure build via environment
 #!/bin/bash
 
+# Set umask so new files/directories get full permissions automatically
+umask 000
+
 TASMOTA_VOLUME='/tasmota'
 
-
 if [ -d $TASMOTA_VOLUME ]; then
-	cd $TASMOTA_VOLUME
-	echo "Compiling Tasmota..."
-	pio run $@
-	echo "All done! Find your builds in Tasmota/build_output/firmware/"
+    cd $TASMOTA_VOLUME
+    echo "Compiling Tasmota..."
+    pio run $@
+    echo "All done! Find your builds in Tasmota/build_output/firmware/"
 else
-	echo ">>> NO TASMOTA VOLUME MOUNTED --> EXITING"
-	exit 0;
+    echo ">>> NO TASMOTA VOLUME MOUNTED --> EXITING"
+    exit 0;
 fi
